83f2c8b6625242f0c93ba5079bf8d1218cfe08cd
root <root@vikmak.fvds.ru>
2020-03-17 06:48:45 -0400

    11. deploying

--- resources/views/parts/navigation.blade.php
+++ resources/views/parts/navigation.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <a class="navbar-brand" href="/">
-        <img style="max-height: 50px; max-width: 50px; filter: invert(100%)" src="{{ asset("storage/cluster-icon.png") }}" alt="LinksManagement Logo">
+        <img style="max-height: 50px; max-width: 50px; filter: invert(100%)" src="{{ asset('storage/cluster-icon.png') }}" alt="LinksManagement Logo">
         Статистика доменов
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
