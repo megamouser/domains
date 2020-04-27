0b64372cff95111cc16813c3d424d5eda44097a3
Artem Yurev <artemon-yrev@yandex.ru>
2020-04-27 18:19:57 +0300

    collecting statistic part2

--- app/Console/Commands/GrabParams.php
+++ app/Console/Commands/GrabParams.php
@@ -46,21 +46,29 @@ class GrabParams extends Command
         $currentProcessId = DB::table("processes")->insertGetId(["name" => "collecting-$numberArgument", "status" => "runned", "runned_at" => date("Y-m-d H:i:s")]);
 
         $domainsWithoutParams = DB::table("domains")->where("json_params")->get();
-        $domainsWithoutParams = $domainsWithoutParams->chunk("10000")[$numberArgument];
-        foreach ($domainsWithoutParams as $key => $domainWithoutParams) 
+
+        while ($domainsWithoutParams) 
         {
             try {
-                $client = new Client();
-                $response = $client->request('GET', 'https://seo-rank.my-addr.com/api2/moz+alexa+sr+fb/1BAFA8ED4032A9DAFE1DEB9D0BD6AE6F/' . $domainWithoutParams->name);
-                $json = json_encode(json_decode($response->getBody()));
-                DB::table('domains')->where('name', $domainWithoutParams->name)->update(["name" => $domainWithoutParams->name, "da" => json_decode($json)->da, "pa" => json_decode($json)->pa, "moz" => json_decode($json)->mozrank, "links" => json_decode($json)->links, "equity" => json_decode($json)->equity, "json_params" => $json]);
-                // dump($domainWithoutParams->name);
+                $domainsWithoutParams = $domainsWithoutParams->chunk("500")[$numberArgument];
             } catch (\Throwable $th) {
-                // dump($th);
+                DB::table("processes")->truncate();
             }
-        }
 
-        DB::table("processes")->where("id", $currentProcessId)->update(["status" => "stopped", "stopped_at" => date("Y-m-d H:i:s")]);
-        dd("end: " . $this->argument("number"));
+            foreach ($domainsWithoutParams as $key => $domainWithoutParams) 
+            {
+                try {
+                    $client = new Client();
+                    $response = $client->request('GET', 'https://seo-rank.my-addr.com/api2/moz+alexa+sr+fb/1BAFA8ED4032A9DAFE1DEB9D0BD6AE6F/' . $domainWithoutParams->name);
+                    $json = json_encode(json_decode($response->getBody()));
+                    DB::table('domains')->where('name', $domainWithoutParams->name)->update(["name" => $domainWithoutParams->name, "da" => json_decode($json)->da, "pa" => json_decode($json)->pa, "moz" => json_decode($json)->mozrank, "links" => json_decode($json)->links, "equity" => json_decode($json)->equity, "json_params" => $json]);
+                } catch (\Throwable $th) {
+                    dump($th);
+                }
+            }
+    
+            DB::table("processes")->where("id", $currentProcessId)->update(["status" => "stopped", "stopped_at" => date("Y-m-d H:i:s")]);
+            dd("end: " . $this->argument("number"));
+        }
     }
 }
--- resources/views/statistic/index.blade.php
+++ resources/views/statistic/index.blade.php
@@ -9,16 +9,31 @@
         <h1>Collect statistics</h1>
     </div>
 
-    <div class="container">
-        <span>Domain names without statistics: </span><span class="badge badge-pill badge-info p-2">{{ $itemsCount }}</span>
-    </div>
+    @if ($itemsCount)
+        <div class="container">
+            <span>Domain names without statistics: </span><span class="badge badge-pill badge-info p-2">{{ $itemsCount }}</span>
+        </div>
 
-    <div class="container">
-        @if ($collectingAllowed)
-            <button class="btn btn-success get-statistic">Start collecting</button>
-        @else
-            <button disabled class="btn btn-success get-statistic">Collecting statistic ... </button>
-        @endif
-    </div>
+        <div class="container">
+            @if ($collectingAllowed)
+                <button class="btn btn-success get-statistic">Start collecting</button>
+            @else
+                <button disabled class="btn btn-success get-statistic">Collecting statistic ... </button>
+            @endif
+        </div>
+    @else
+        <div class="container">
+            <span>Domain names without statistics:</span>
+            <span class="badge badge-pill badge-info p-2">{{ $itemsCount }}</span>
+        </div>
+
+        <br>
+
+        <div class="container">
+            <div class="alert alert-warning">
+                <span>Domains without statistic params not found</span>
+            </div>
+        </div>
+    @endif
 </div>
 @endsection
